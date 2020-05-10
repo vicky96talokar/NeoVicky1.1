b384dd8252d54080e0d42c8fdcc762af0c5f71ba
vicky <vivek.talokar@gmail.com>
2020-05-10 16:07:59 +0530

    new items added

---
+++ NeoVicky/NeoVicky/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NeoVicky.Models;
+
+namespace NeoVicky.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Matrix"
+            };
+            return View(movie);
+        }
+    }
+}
