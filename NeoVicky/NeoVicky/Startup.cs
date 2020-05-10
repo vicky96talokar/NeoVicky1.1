10359995f6bbd562cd3012c27a996558cae2c39d
vicky <vivek.talokar@gmail.com>
2020-05-10 14:32:09 +0530

    new added

---
+++ NeoVicky/NeoVicky/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NeoVicky.Startup))]
+namespace NeoVicky
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
