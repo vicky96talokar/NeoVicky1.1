6907484dde6a6adb022002fb2d4ed5e32ab2c962
vicky <vivek.talokar@gmail.com>
2020-05-19 17:31:13 +0530

    new added

---
+++ NeoVicky/NeoVicky/Migrations/202005191150560_Initialneo111.cs
@@ -0,0 +1,16 @@
+﻿namespace NeoVicky.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initialneo111 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- NeoVicky/NeoVicky/ViewModels/RandomMovieViewModel.cs
+++ NeoVicky/NeoVicky/ViewModels/RandomMovieViewModel.cs
@@ -1,8 +1,8 @@
-﻿using NeoVicky.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using NeoVicky.Models;
 
 namespace NeoVicky.ViewModels
 {
